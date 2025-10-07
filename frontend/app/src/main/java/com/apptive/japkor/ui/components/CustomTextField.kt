99a52a6a0510e4c7c15fa5c7ff628656e0b900b0
BYEONGCHAN LEE <mark77234@naver.com>
2025-10-07 22:02:43 +0900

    feat - 로그인 및 회원가입 API 연동 (#18)
    * feat: 공용 텍스트필드 개선
    
    * feat: 로그인 API 추가 및 적용, DTO, 커스텀 토스트 추가
    
    * feat: 네비게이터 수정
    
    * feat: 비밀번호 hide 추가
    
    * feat: 텍스트 수직 정렬
    
    * feat: 회원가입 DTO 및 response 수정
    
    * feat: 회원가입 페이지 추가 및 API 연동
    
    * feat: UI 개선
    
    * feat: 라우팅 개선

--- frontend/app/src/main/java/com/apptive/japkor/ui/components/CustomTextField.kt
+++ frontend/app/src/main/java/com/apptive/japkor/ui/components/CustomTextField.kt
@@ -1,12 +1,17 @@
 package com.apptive.japkor.ui.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.apptive.japkor.ui.theme.CustomColor
 
@@ -14,26 +19,31 @@ import com.apptive.japkor.ui.theme.CustomColor
 fun CustomOutlinedTextField(
     value: String,
     onValueChange: (String) -> Unit,
-    placeholder: String
+    placeholder: String,
+    isPassword: Boolean = false
 ) {
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
         modifier = Modifier
             .fillMaxWidth()
-            .height(50.dp)
-            .border(
-                width = 1.dp,
-                color = CustomColor.gray100,
-                shape = RoundedCornerShape(10.dp)
-            ),
-        shape = RoundedCornerShape(10.dp),
+            .height(50.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = OutlinedTextFieldDefaults.colors(
+            unfocusedBorderColor = CustomColor.gray200,
+            focusedBorderColor = CustomColor.gray300
+        ),
         placeholder = {
             CustomText(
                 text = placeholder,
                 type = CustomTextType.bodyMedium,
                 color = CustomColor.gray300
             )
-        }
+        },
+        textStyle = androidx.compose.ui.text.TextStyle(
+            textAlign = TextAlign.Start
+        ),
+        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = if (isPassword) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default
     )
 }
