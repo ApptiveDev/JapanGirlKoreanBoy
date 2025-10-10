afd250d67d2547d16c82ad7a364ccc2bd30b539e
LEE YERAM <yeram20914@gmail.com>
2025-10-10 16:55:27 +0900

    feat - step3 화면 구현 (#19)
    * feat - step3 화면 구현
    
    * 코드 리뷰 반영

--- frontend/app/src/main/java/com/apptive/japkor/ui/requiredinfo/steps/Step3Content.kt
+++ frontend/app/src/main/java/com/apptive/japkor/ui/requiredinfo/steps/Step3Content.kt
@@ -1,16 +1,20 @@
 package com.apptive.japkor.ui.requiredinfo.steps
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.apptive.japkor.ui.components.CustomOutlinedTextField
 import com.apptive.japkor.ui.components.CustomText
 import com.apptive.japkor.ui.components.CustomTextType
 import com.apptive.japkor.ui.theme.CustomColor
@@ -37,11 +41,36 @@ fun Step3Content() {
         Spacer(modifier = Modifier.height(50.dp))
 
         // 프로필 사진 등록 UI 구현 (예시)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White, shape = RoundedCornerShape(16.dp)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            CustomOutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = "학력(인증)"
+            )
+            CustomOutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = "재산(인증)"
+            )
+            CustomOutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = "기타정보"
+            )
+
+        }
+
         CustomText(
-            text = "Step 3: 화면",
+            text = "'학력' 및 '재산' 정보는 선택 입력 사항입니다.\n입력시, 더 빠르고 정확한 매칭에 도움을 줄 수 있습니다.",
             color = CustomColor.gray300,
             type = CustomTextType.bodyLarge,
+            modifier = Modifier.padding(horizontal = 7.dp)
         )
-        Spacer(modifier = Modifier.height(200.dp))
     }
 }
